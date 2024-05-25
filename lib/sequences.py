69d9d4ee481cf79b10a731e6d4e122f71cc4e06b
Matthew <matthewtndirangu@gmail.com>
2024-05-25 10:24:40 +0300

    attempt lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    a, b = 0, 1
+    result = []
+    for _ in range(length):
+        result.append(a)
+        a, b = b, a + b
+
+    print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
